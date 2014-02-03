1156a5555bddce8d5986fffa4c9b6227f6bd0694
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-03 10:57:03 +0200

    Render all contacts in contacts view

--- app/js/views/contacts.js
+++ app/js/views/contacts.js
@@ -1,9 +1,17 @@
 ContactManager.Views.Contacts = Backbone.View.extend({
   template: _.template($('#tpl-contacts').html()),
 
+  renderOne: function(contact) {
+    var itemView = new ContactManager.Views.Contact({model: contact});
+    this.$('.contacts-container').append(itemView.render().$el);
+  },
+
   render: function() {
     var html = this.template();
     this.$el.html(html);
+
+    this.collection.each(this.renderOne, this);
+
     return this;
   }
 });
