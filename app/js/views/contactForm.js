0eba535f58dd64efdd01b86ff360570604447e05
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-03 11:56:01 +0200

    Trigger event on form submit

--- app/js/views/contactForm.js
+++ app/js/views/contactForm.js
@@ -1,8 +1,23 @@
 ContactManager.Views.ContactForm = Backbone.View.extend({
   template: _.template($('#tpl-new-contact').html()),
+
+  events: {
+    'submit .contract-form': 'onFormSubmit'
+  },
+
   render: function() {
     var html = this.template();
     this.$el.append(html);
     return this;
+  },
+
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    this.trigger('form:submitted', {
+      name: this.$('.contact-name-input').val(),
+      tel: this.$('.contact-tel-input').val(),
+      email: this.$('.contact-email-input').val()
+    });
   }
 });
