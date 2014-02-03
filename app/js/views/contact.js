de8a1992eca20fb0a6286e39b537737312b76851
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-03 11:28:28 +0200

    Add dummy delete button

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -3,9 +3,18 @@ ContactManager.Views.Contact = Backbone.View.extend({
   className: 'media col-md-6 col-lg-4',
   template: _.template($('#tpl-contact').html()),
 
+  events: {
+    'click .delete-contract': 'onClickDelete'
+  },
+
   render: function() {
     var html = this.template(this.model.toJSON());
     this.$el.append(html);
     return this;
+  },
+
+  onClickDelete: function(e) {
+    e.preventDefault();
+    console.log('Delete');
   }
 });
