9c01c240c114f1c6c1ebd832a1de83602807078d
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-03 10:54:00 +0200

    Add template for Contact view

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -1,7 +1,11 @@
 ContactManager.Views.Contact = Backbone.View.extend({
+  tagName: 'li',
+  className: 'media col-md-6 col-lg-4',
+  template: _.template($('#tpl-contact').html()),
+
   render: function() {
-    var html = '<h1>' + this.model.get('name') + '</h1>';
-    this.$el.html(html);
+    var html = this.template(this.model.toJSON());
+    this.$el.append(html);
     return this;
   }
 });
